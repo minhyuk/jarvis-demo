d79dda6da23ac2584bbce312b79bbcfa97741e4d
CT-APR <ct_apr@gmail.com>
2025-01-09 16:33:34 +0900

    applying jarvis patch

--- bad_case.c
+++ bad_case.c
@@ -10,13 +10,13 @@ int test1603(int x)
 		break;
 	case 3:	
 		x--;
+            break;
 	default:
-	    ;
+            break;
 	}
 
 	return x;
 }
-
 short test0902(int x, int y, short e){
    short buf[ 3 ][ 2 ] = { 1, 2, 0, 0, 5, 6 }; // MISRA_C_2012_09_02
    buf[x][y] = e;
