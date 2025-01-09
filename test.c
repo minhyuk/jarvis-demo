59f339a72484897db1f957648c64a4e31d9aa5b6
Minhyuk Kwon <minhyuk@suresofttech.com>
2025-01-09 16:47:00 +0900

    testcase1

--- test.c
+++ test.c
@@ -1,26 +1,13 @@
-#include <assert.h>
-#include "bad_case.h"
+#include <stdio.h>
 
-int main() {
+int test(int weight) {
+    int a = 1;
+    float b = 2.0;
+    return a + b + weight;
+}
 
-    short test00 = test0902(0,0,92);
-    short test01 = test0902(0,1,91);
-    short test10 = test0902(1,0,90);
-    short test11 = test0902(1,1,89);
-    short test20 = test0902(2,0,88);
-    short test21 = test0902(2,1,87);
-
-    assert(test00 == (short)92);
-    assert(test01 == (short)91);
-    assert(test10 == (short)90);
-    assert(test11 == (short)89);
-    assert(test20 == (short)88);
-    assert(test21 == (short)87);
-
-    assert(test1603(1) == 2);
-    assert(test1603(2) == 3);
-    assert(test1603(3) == 2);
-    assert(test1603(4) == 4);
-	
-	return 0;
+int main(int argc, char *argv[]) {
+    float weight = test(argc);
+    printf("%f", weight);
+    return 0;
 }
