d3aecd85800489a2f6ff4e9041b37af3bbaf8bae
CT-APR <ct_apr@gmail.com>
2025-01-17 13:31:52 +0900

    applying jarvis patch

--- test.c
+++ test.c
@@ -1,8 +1,8 @@
 
 int test(int weight) {
     int a = 1;
-    float b = 2.0;
-    return a + b + weight;
+    float b = 2.0f; // Ensure the float is specified with 'f'
+    return a + static_cast<int>(b) + weight; // Cast 'b' to 'int'
 }
 
 int main(int argc, char *argv[]) {
