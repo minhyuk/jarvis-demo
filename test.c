e2e5f00a886ae141a0616f152e16f7ec6d9f9548
Minhyuk Kwon <minhyuk@suresofttech.com>
2025-01-17 09:18:29 +0900

    'revise'

--- test.c
+++ test.c
@@ -1,4 +1,3 @@
-int printf(const char *format, ...);
 
 int test(int weight) {
     int a = 1;
@@ -8,6 +7,5 @@ int test(int weight) {
 
 int main(int argc, char *argv[]) {
     float weight = test(argc);
-    //printf("%f", weight);
     return 0;
 }
