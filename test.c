01423a9d40b62185a3033456ec1b9a95c141f8d5
CT-APR <ct_apr@gmail.com>
2025-01-17 14:27:30 +0900

    applying jarvis patch

--- test.c
+++ test.c
@@ -1,11 +1,19 @@
 
 int test(int weight) {
     int a = 1;
-    float b = 2.0;
-    return a + b + weight;
+    float b = 2.0f;
+    return a + (int)b + weight; // Cast b to int to ensure the same type category
+}
+
+#include <stdio.h>
+
+float test(int value) {
+    // Dummy implementation
+    return (float)value;
 }
 
 int main(int argc, char *argv[]) {
+    (void)argv; // Explicitly mark argv as unused
     float weight = test(argc);
     return 0;
 }
